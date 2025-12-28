43aa279646607561e56a220113183fd4af36d8d5
Guilherme Araújo <132411540+Slotov7@users.noreply.github.com>
2025-12-28 13:23:56 -0300

    feat: adiciona componente `TechSelect` para seleção múltipla de tecnologias com busca e exibição de tags.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,34 @@
 import Header from "@/components/homePage/HeaderHome";
 import SobreSection from "@/components/homePage/SobreSection";
-import FooterHome from "@/components/homePage/FooterHome";
 import HeroSection from "@/components/homePage/HeroSection";
 import TecnologiasSection from "@/components/homePage/TecnologiasSection";
 import ContactSection from "@/components/homePage/ContactSection";
 import ProjectsSection from "@/components/homePage/ProjectsSection";
 import Footer from "@/components/Footer";
 
+// Desabilitar cache para sempre buscar dados atualizados
+export const revalidate = 0;
+
 export default async function Home() {
+  const navLinks = [
+    { href: "#projetos", label: "Projetos" },
+    { href: "#tecnologias", label: "Tecnologias" },
+    { href: "#sobre", label: "Sobre" },
+    { href: "#contato", label: "Contato" },
+  ];
 
+  return (
+    <div className="bg-gradient-to-b from-[#060606] to-[#2C2A2A] text-white flex flex-col h-screen">
+      <Header />
 
-    const navLinks = [
-        {href: "#projetos", label: "Projetos"},
-        {href: "#tecnologias", label: "Tecnologias"},
-        {href: "#sobre", label: "Sobre"},
-        {href: "#contato", label: "Contato"},
-    ];
-
-    return (
-        <div className="bg-gradient-to-b from-[#060606] to-[#2C2A2A] text-white flex flex-col h-screen">
-            <Header />
-
-            <main className="flex-1 overflow-x-hidden w-full">
-                <HeroSection/>
-                <ProjectsSection/>
-                <TecnologiasSection/>
-                <SobreSection/>
-                <ContactSection/>
-                <Footer/>
-            </main>
-
-
-        </div>
-    );
+      <main className="flex-1 overflow-x-hidden w-full">
+        <HeroSection />
+        <ProjectsSection />
+        <TecnologiasSection />
+        <SobreSection />
+        <ContactSection />
+        <Footer />
+      </main>
+    </div>
+  );
 }
